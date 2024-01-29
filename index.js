4f7258f5a2a326abce69312be0a94ff252ea94f3
BRC-Thomas <thomasbaracani@gmail.com>
2024-01-29 13:20:29 +0100

    done

--- index.js
+++ index.js
@@ -1,24 +1,55 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const bodyParser = require("body-parser");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+const urlDatabase = [];
+let currentId = 1;
+
+
+app.post("/api/shorturl", (req, res) => {
+  const url = req.body.url; 
+  const regex = /^http:\/\/www\.[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$/;
+
+  if (!regex.test(url)) {
+    res.json({ error: 'invalid url' });
+  } 
+
+  const shortUrl = currentId;
+  urlDatabase.push({ url, shortUrl });
+  currentId++;
+
+
+  res.json({ original_url: url, short_url: shortUrl });
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-app.listen(port, function() {
+app.get("/api/shorturl/:short_url", function (req, res) {
+  const shortUrl = parseInt(req.params.short_url);
+
+  const urlEntry = urlDatabase.find(entry => entry.shortUrl === shortUrl);
+
+  if(urlEntry){
+    console.log(urlEntry);
+    res.redirect(urlEntry.url)
+  } else {
+    res.json({ error: "short_url not found" });
+  }
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
